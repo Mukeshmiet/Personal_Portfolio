fe91da2effc099f0b173667d7edc319d2a4a94cc
Mukesh Kumar <mukesh.kumar.me.2018@miet.ac.in>
2023-08-24 16:31:10 +0530

    static file changes

--- myportfolio/myportfolio/settings.py
+++ myportfolio/myportfolio/settings.py
@@ -161,9 +161,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "staticfiles"
 STATIC_URL = 'static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = path.join(BASE_DIR, "staticfiles")   
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 STORAGES = {
     # Enable WhiteNoise's GZip and Brotli compression of static assets:
